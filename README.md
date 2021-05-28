5e1da2bdcaae5559f23c6b139acc3d1db201acaa
xeis <xeisbog@gmail.com>
2021-05-28 23:07:33 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# DiscordNitro
+This is a bot to get discord Nitro Free, you only need patience
+
+Steps:
+
+First, download the essential:
+Node.js and a editor like Notepad++
+
+go to the folder of the project and use Shift + Right click and press in open power shell
+
+Type node install discord.js
+wait
+type npm install generate-password
+
+now type node bot.js
+
+go to any channel of your server and type -help
+
+Enjoy!
---
+++ bot.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const generator = require('generate-password');
+
+client.on('ready', () => {
+ client.user.setActivity("Generating Nitro!");
+ console.log("Bot ("+client.user.tag+") Ready!");
+ });
+ 
+client.on("message", async message => {
+  const prefix = "-";
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  if(command === "help"){
+	var i = 0;
+    while (i<1000) { //Number of messages
+    var password = generator.generate({
+    length: 16,
+    numbers: true
+});
+	message.channel.send("https://discord.gift/" + password)
+    i++;
+	}
+  }
+})
+
+client.login('ODQ3OTQwMTQ0NjI2OTI1NjE4.YLFXzA.VEu41Ft7c2GiWCv8zVqasHbax08');
